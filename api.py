4edf041b8fd831be0518389642433bd12261489f
liuxq <liuxq@pchilltech.com>
2018-05-09 17:46:57 +0800

    update api.py

--- api.py
+++ api.py
@@ -21,19 +21,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir ,'da
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# email
-app.config['MAIL_SERVER'] = 'smtp.126.com'
-app.config['MAIL_PORT'] = 25
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-
-
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
-mail = Mail(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
